6fe28611fa0b18761c7c44785697b9c3961f436d
Victor Machado <victorviniciusmachado@yahoo.com.br>
2024-09-30 21:30:15 -0300

    Módulo 12 - Aula 3

--- cypress/e2e/3 - loja-ebac/detalhes-conta.cy.js
+++ cypress/e2e/3 - loja-ebac/detalhes-conta.cy.js
@@ -9,8 +9,14 @@ describe('Funcionalidade: Detalhes da conta', () => {
         })
     });
 
-    it('Deve completar detalhes da conta com sucesso', () => {
+    it('Deve completar detalhes da conta com sucesso - automatizado', () => {
         cy.detalhesConta('Victor', 'Machado', 'Veve1991')
         cy.get('.woocommerce-MyAccount-content > :nth-child(3)').should('contain', 'A partir do painel de controle de sua conta, você pode ver suas compras recentes, gerenciar seus endereços de entrega e faturamento, e editar sua senha e detalhes da conta.')
     });
 });
+
+/*Neste exemplo o cypress vai visitar a loja Ebac em detalhes da conta, e para acessar este campo é necessário logar primeiro.
+Sendo assim ser]ao utilizados os comandos: 
+- cy.visit (como o site loja ebac já está cadastrado em cypress.config.js, basta incluir o campo que deseja acessar),
+- cy.fixture: pois já cadastramos um login e senha padrão no fixture (pasta perfil.json);
+e após realizar o login será utilizado o comando automatizado cy.detalhesConta */
--- cypress/e2e/3 - loja-ebac/produtos.cy.js
+++ cypress/e2e/3 - loja-ebac/produtos.cy.js
@@ -1,17 +1,36 @@
 ///<reference types="cypress"/>
+import produtosPage from "../../support/page-objects/produtos.page";
 
 describe('Funcionalidade: Produtos', () => {
         beforeEach(() => {
-            cy.visit('produtos/')
+           // cy.visit('produtos')
+            produtosPage.visitarUrl()
         });
-     it('Deve selecionar um produto da lista', () => {
-        cy.get('.product-block')
-            //.first() Pega o primeiro elemento da lista
+     
+     
+        it('Deve selecionar um produto da lista', () => {
+            produtosPage.buscarProdutoLista('Abominable Hoodie')
+       /* cy.get('.product-block')
+            .first() Pega o primeiro elemento da lista
             //.last() Pega o ultimo elemento da lista
             //.eq(2) Pega um elemento aleatório da lista, informando o numero que o produto se encontra na web
-            .contains('Abominable Hoodie')
-            .click()
+            //.contains('Abominable Hoodie')
+            .click()*/
 
             cy.get('#tab-title-description > a').should('contain','Descrição')
 });
-    })
+ it.only('Deve buscar um produto com sucesso', () => {
+    let produto = 'Aether Gym Pant'
+    produtosPage.buscarProduto(produto)
+    cy.get('.product_title').should('contain', produto)
+ });
+
+ it('Deve visitar a página do produto', () => {
+    
+ });
+
+ it('Deve adicionar produto ao carrinho', () => {
+    
+ });
+
+    });
